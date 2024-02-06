7f1010f42bfd61069a95619565965187e0e03435
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-02-06 17:29:59 -0300

    Add clsx library and category button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,15 @@
+import { CatergoryButton } from '@/components/catergory-button'
 import { Header } from '@/components/header'
 import { View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-950">
+    <View className="flex-1 bg-slate-950">
       <Header title="Cardápio" cartQuantity={1} />
+
+      <View className="flex-row gap-4">
+        <CatergoryButton title="Todos" isSelected />
+      </View>
     </View>
   )
 }
---
+++ src/components/catergory-button.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx'
+import React from 'react'
+import { Pressable, PressableProps, Text } from 'react-native'
+
+type CatergoryButtonProps = PressableProps & {
+  title: string
+  isSelected?: boolean
+}
+
+export function CatergoryButton({
+  title,
+  isSelected = false,
+  ...rest
+}: CatergoryButtonProps) {
+  return (
+    <Pressable
+      className={clsx(
+        'bg-slate-800 px-4 justify-center rounded-md h-10',
+        isSelected && 'border-2 border-lime-300',
+      )}
+      {...rest}
+    >
+      <Text className="text-slate-100  font-subtitle text-sm">{title}</Text>
+    </Pressable>
+  )
+}
